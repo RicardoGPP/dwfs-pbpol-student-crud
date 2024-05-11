66d078597d93e9643e28f9897e9139bbfcb6b5a8
Ricardo Giovani Piantavinha Perandré <ricardo.piantavinha@gmail.com>
2024-05-11 10:16:20 -0300

    Model structure changed

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
-from .models.aluno import Aluno
+from django.db import models
+
+class Aluno(models.Model):
+
+    matricula = models.PositiveIntegerField(unique=True)
+    data_nascimento = models.DateField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    data_ingresso = models.DateField()
